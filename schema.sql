d9f6003a7941250b670f4a1652419e1f760a9bcd
CHRIS HAMERSLY <CHRISTOPHERHAMERSLY@GMAIL.COM>
2020-06-11 06:47:34 -0700

    worked out bugs

--- schema.sql
+++ schema.sql
@@ -9,11 +9,7 @@ CREATE TABLE location (
 );
 
 INSERT INTO location (search_query, formatted_query, latitude, longitude) VALUES ('seattle', 'Seattle, King County, Washington, USA', '47.6038321' , '-122.3300624');
--- SELECT * FROM location;
+SELECT * FROM location;
 
 
 
--- Don't worry about inserting ID - does this automatically
--- To test: Keep trying to search for a city you know is in the DB and see if it keeps calling to the API (shouldn't be if it's happening correctly)
--- Put a console.log ABOVE and INSIDE your superagent call and see which one gets hit each time
--- As soon as that works, THEN go to the INSERT - but initial check needs to work in the first place
